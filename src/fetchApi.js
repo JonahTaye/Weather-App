4649bcba540bb6d21c4efe1df5f99ab3a070492c
jonah <jonah.softwaredev@gmail.com>
2025-05-05 10:15:10 +0300

    feat: add to fetching functions

---
+++ src/fetchApi.js
@@ -0,0 +1,13 @@
+export async function fetchWeather (cityOrCountry, unit) {
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityOrCountry}?unitGroup=${unit}&key=8S4X6VXE6T99EPF7D3Y5D6YJV&contentType=json`
+  )
+  const weather = await response.json()
+  return weather
+}
+
+export async function fetchLocation () {
+  const response = await fetch('https://ipapi.co/json/')
+  const location = await response.json()
+  return location
+}
