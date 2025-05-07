959a36d0d0f2b2b220a3c311f64710352782d85d
jonah <jonah.softwaredev@gmail.com>
2025-05-07 22:39:34 +0300

    feat: add error handling for 400 response

--- src/fetchApi.js
+++ src/fetchApi.js
@@ -1,13 +1,21 @@
+import { displayError } from './display'
+
 export async function fetchWeather (cityOrCountry, unit) {
   const response = await fetch(
-    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityOrCountry}?unitGroup=${unit}&key=8S4X6VXE6T99EPF7D3Y5D6YJV&contentType=json`
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityOrCountry}?unitGroup=${unit}&key=8S4X6VXE6T99EPF7D3Y5D6YJV&contentType=json`,
+    { mode: 'cors' }
   )
-  const weather = await response.json()
-  return weather
+
+  if (!response.ok) {
+    displayError()
+  } else {
+    const weather = await response.json()
+    return weather
+  }
 }
 
 export async function fetchLocation () {
   const response = await fetch('https://ipapi.co/json/')
   const location = await response.json()
-  return location
+  return location.city
 }
