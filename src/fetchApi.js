9641d0aef309d79adced318cf7e810acb7aacd82
jonah <jonah.softwaredev@gmail.com>
2025-05-08 11:04:01 +0300

    feat: add error handling and loading screen

--- src/fetchApi.js
+++ src/fetchApi.js
@@ -1,13 +1,15 @@
-import { displayError } from './display'
+import { displayError, displayLoading } from './display'
 
 export async function fetchWeather (cityOrCountry, unit) {
+  displayLoading()
   const response = await fetch(
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${cityOrCountry}?unitGroup=${unit}&key=8S4X6VXE6T99EPF7D3Y5D6YJV&contentType=json`,
     { mode: 'cors' }
   )
-
+  console.log(response)
   if (!response.ok) {
     displayError()
+    return 'Error'
   } else {
     const weather = await response.json()
     return weather
